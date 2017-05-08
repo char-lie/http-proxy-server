8dc74c9b60eda9a7b71a52e29a3787e12730fdae
char-lie <5962274+char-lie@users.noreply.github.com>
2017-05-08 23:55:43 +0300

    Added main file with ExpressJS server

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import proxyRequest from './proxy';
+
+const app = express();
+
+app.get('/:url', (req, res) => {
+  proxyRequest(req.params.url).then(({ status, body, headers }) => {
+    res.status(status);
+    res.set(headers);
+    res.send(body);
+  }).catch(e => {
+    res.status(500)
+    res.send(`An error occured ${e}`);
+  });
+})
+
+app.listen(3000);
