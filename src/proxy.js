6349b2c54ee1b09c6ec1cb22bad3cc3b248f72c3
char-lie <5962274+char-lie@users.noreply.github.com>
2017-05-08 23:55:20 +0300

    Added proxy mechanism code
    Requests body from needed site and returns result

---
+++ src/proxy.js
@@ -0,0 +1,26 @@
+import http from 'http';
+
+export default function(hostname) {
+  return new Promise((fulfill, reject) => {
+    http.get({
+      hostname,
+      port: 80,
+      path: '/',
+      agent: false,
+    }, response => {
+      let body = '';
+      response.on('data', chunk => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        fulfill({
+          body,
+          headers: response.headers,
+          status: response.statusCode,
+        })
+      });
+    }).on('error', e => {
+      reject(e);
+    });
+  });
+};
