1c3afd710ccf7b79987f2f9ef834a4981a0110c9
char-lie <5962274+char-lie@users.noreply.github.com>
2017-05-09 09:59:04 +0300

    Refactored index file and fixed errors handling
    If error occurred, user should not see it -- we need to log it

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import proxyRequest from './proxy';
 const app = express();
 
 app.get('/:url', (req, res) => {
-  proxyRequest(req.params.url).then(({ status, body, headers }) => {
-    res.status(status);
-    res.set(headers);
-    res.send(body);
-  }).catch(e => {
-    res.status(500)
-    res.send(`An error occurred ${e}`);
-  });
+  proxyRequest(req.params.url)
+    .then(({ status, body, headers }) => {
+      res.status(status);
+      res.set(headers);
+      res.send(body);
+    })
+    .catch(e => {
+      console.error(`${new Date()}: ${e}`);
+      res.status(500)
+      res.send();
+    });
 })
 
 app.listen(3000);
