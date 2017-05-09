0625598e2a7f5b5ced1be6f4e5e6be7e792779ce
char-lie <5962274+char-lie@users.noreply.github.com>
2017-05-09 09:55:52 +0300

    Fixed watch task in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 const webpackConfig = require('./webpack.config.js');
+const path = require('path');
 
 module.exports = function (grunt) {
   grunt.initConfig({
@@ -7,13 +8,28 @@ module.exports = function (grunt) {
     },
     watch: {
       app: {
-        files: ['index.js', 'proxy.js'],
+        files: ['src/*.js'],
         tasks: ['webpack:build'],
+      },
+    },
+    nodemon: {
+      dev: {
+        script: path.join(webpackConfig.output.path,
+                          webpackConfig.output.filename),
+        },
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true,
+        }
       }
     },
   });
-  grunt.loadNpmTasks('grunt-webpack');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('watch', ['webpack:build', 'watch:app']);
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 }
